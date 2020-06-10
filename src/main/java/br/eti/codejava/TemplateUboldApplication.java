eada38ce2abaf8f88f1261b8be47da8bc1640cba
Clovison Rodrigues da Costa <clovison.ti@gmail.com>
2020-06-10 20:21:21 -0300

    template ubold

---
+++ src/main/java/br/eti/codejava/TemplateUboldApplication.java
@@ -0,0 +1,13 @@
+package br.eti.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateUboldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateUboldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/eti/codejava/TemplateUboldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.codejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateUboldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
